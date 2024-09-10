1384ab5ac2e980a2e4b4445c43be655bc7bf1f2f
Kai <kaiximenes@outlook.com>
2024-09-10 13:44:37 -0300

    Aula 1 - finalizada

---
+++ index.js
@@ -0,0 +1,16 @@
+
+let meta = {
+    value: 'ler um livro por mes',
+    checked: true
+}
+
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos todos os dias",
+        checked: false
+    }
+]
+
+console.log(metas[1].value);
+
